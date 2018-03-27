b4eadf30b882fbd2e3049a61ed2155fc085e2ff7
Susan_Dymek <scdymek@msn.com>
2018-03-27 10:09:03 -0700

    Updated settings.py to include log in app and expense tracker app

--- money_coach_project/settings.py
+++ money_coach_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.expense_tracker_app',
+    'apps.log_reg_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
